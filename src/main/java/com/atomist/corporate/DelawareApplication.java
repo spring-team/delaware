8304de7ce5d66fa5832460c4254c65ca83560f93
Rod Johnson <johnsonroda@gmail.com>
2018-02-14 06:39:43 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/corporate/DelawareApplication.java
@@ -0,0 +1,12 @@
+package com.atomist.corporate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelawareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelawareApplication.class, args);
+	}
+}
---
+++ src/test/java/com/atomist/corporate/DelawareApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atomist.corporate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DelawareApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
